e67be6e584427e7224b14f80b0f617ded743bb46
Maxou <github@paulmaxime.fr>
2023-09-30 21:48:25 +0200

    Rename the GatheringManager, allow increasing the amount gathered

--- scripts/GatheringManager.gd
+++ scripts/GatheringManager.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var click_particles: PackedScene
 
+var gathering_speed = 1
+
 func _ready():
 	$'../EventManager'.block_clicked.connect(on_block_clicked)
 
@@ -10,6 +12,8 @@ func on_block_clicked(block_type, _layer, _coordinate, screen_coordinate, _wall_
 		var particles: CPUParticles2D = click_particles.instantiate()
 		particles.position = screen_coordinate
 		particles.emitting = true
+		particles.amount = gathering_speed
 		add_child(particles)
 		get_tree().create_timer(particles.lifetime).timeout.connect(particles.queue_free)
-		$'../../ResourcesManager'.update_sand(1)
+		$'../../ResourcesManager'.update_sand(gathering_speed)
+		gathering_speed += 1
