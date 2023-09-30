67630fc7a8cc71ba349cf9852653d35a5ec3285a
Gerardufoin <gerardufoin2@hotmail.com>
2023-09-30 11:49:04 +0200

    Add the finished project. Probably.

---
+++ scripts/Water.gd
@@ -0,0 +1,28 @@
+extends TileMap
+
+var water_level = -1
+var grid = Vector2i(100, 100)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	increase_water_level()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if (event.pressed):
+			increase_water_level()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func increase_water_level():
+	if (water_level + 1 >= get_layers_count()):
+		pass
+	water_level += 1
+	for x in range(-grid.x / 2, grid.x / 2):
+		for y in range(-grid.y / 2, grid.y / 2):
+			var cell = get_cell_tile_data(water_level, Vector2i(x, y))
+			if (cell == null):
+				set_cell(water_level, Vector2i(x, y), 1, Vector2i(1, 0))
+
