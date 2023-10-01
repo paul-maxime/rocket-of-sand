876f9cf3ad61a5b3f7dc6d6ab0c650a8e8e04b16
Gerardufoin <gerardufoin2@hotmail.com>
2023-10-01 15:51:23 +0200

    Build the rocket at market prices

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
 @export var time_by_layer = 30
+@export var rocket: Node2D
+@export var rocket_states: Array[CompressedTexture2D]
 
 var time = 0
+var death_layer = 0
+var rocket_progress = 0
+var rocket_price = 100
+
 @onready var water = $"../Island"
+@onready var gathering_manager = $'GatheringManager'
+@onready var interface_manager = $'/root/MainScene/CanvasLayer'
+
+func _ready():
+	$'/root/MainScene/CanvasLayer/Panel/BuyRocketButton'.pressed.connect(build_rocket)
+	rocket.get_child(0).texture = null
+	water.the_water_rises.connect(check_game_over)
+	for i in water.get_layers_count():
+		if (water.get_layer_name(i) == 'Game Over'):
+			death_layer = i
 
 func _process(deltaTime):
 	time += deltaTime;
@@ -11,3 +27,15 @@ func _process(deltaTime):
 		time -= time_by_layer
 		water.increase_water_level()
 	water.update_water_offset(1 - time / time_by_layer)
+
+func build_rocket():
+	if (gathering_manager.current_sand >= rocket_price):
+		gathering_manager.add_sand(-rocket_price)
+		rocket_price *= 10
+		interface_manager.update_rocket_price(rocket_price, gathering_manager.current_sand)
+		rocket_progress = min(rocket_progress + 1, rocket_states.size())
+		rocket.get_child(0).texture = rocket_states[rocket_progress - 1]
+
+func check_game_over(water_level):
+	if (water_level == death_layer):
+		rocket.visible = false
--- scripts/InterfaceManager.gd
+++ scripts/InterfaceManager.gd
@@ -3,11 +3,22 @@ extends CanvasLayer
 @onready var sand_label: Label = $"Panel/CenterContainer/SandLabel"
 @onready var buy_drill_button: Button = $"Panel/BuyDrillButton"
 @onready var buy_factory_button: Button = $"Panel/BuyFactoryButton"
+@onready var buy_rocket_button: Button = $"Panel/BuyRocketButton"
+
+var drill_price = 100
+var factory_price = 100
+var rocket_price = 100
 
 func _ready():
 	update_sand(0)
 
 func update_sand(value: int):
 	sand_label.text = str(value)
-	buy_drill_button.disabled = value < 100
-	buy_factory_button.disabled = value < 100
+	buy_drill_button.disabled = value < drill_price
+	buy_factory_button.disabled = value < factory_price
+	buy_rocket_button.disabled = value < rocket_price
+
+func update_rocket_price(price, current_sand):
+	rocket_price = price
+	buy_rocket_button.text = str(rocket_price)
+	buy_rocket_button.disabled = current_sand < rocket_price
