a91a45ad1e544bc345386df728ed3a0d776fafc1
Maxou <github@paulmaxime.fr>
2023-10-02 00:36:21 +0200

    Improve the camera (round + limits)

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -8,6 +8,11 @@ var current_delta = Vector2()
 var previous_click_position = Vector2()
 var is_dragging: bool = false
 
+const min_x = -800
+const max_x = 800
+const min_y = -400
+const max_y = 400
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if !event.is_pressed():
@@ -18,11 +23,15 @@ func _unhandled_input(event):
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP && current_zoom < 10:
 			current_zoom += 1
 			zoom = Vector2(current_zoom, current_zoom)
-		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN && current_zoom > 1:
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN && current_zoom > 2:
 			current_zoom -= 1
 			zoom = Vector2(current_zoom, current_zoom)
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if is_dragging or previous_click_position.distance_to(get_viewport().get_mouse_position()) > DRAG_MIN_DISTANCE:
 			is_dragging = true
 			current_delta += event.relative / current_zoom
-			position = -current_delta
+			if current_delta.x < min_x: current_delta.x = min_x
+			if current_delta.x > max_x: current_delta.x = max_x
+			if current_delta.y < min_y: current_delta.y = min_y
+			if current_delta.y > max_y: current_delta.y = max_y
+			position = (-current_delta).round()
