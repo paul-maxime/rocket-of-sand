491ece3fc9282603d48a77f788d0fcd49ac7a253
Maxou <github@paulmaxime.fr>
2023-10-02 06:25:19 +0200

    Try to pause the game when unfocused on web

--- scripts/PauseManager.gd
+++ scripts/PauseManager.gd
@@ -1,16 +1,13 @@
 extends Node
 
-var tabcheat_callback: JavaScriptObject
-var focuscheat_callback: JavaScriptObject
+var visibility_change_callback: JavaScriptObject
 
 func _ready():
 	if JavaScriptBridge != null:
-		tabcheat_callback = JavaScriptBridge.create_callback(on_tab_cheat)
-		focuscheat_callback = JavaScriptBridge.create_callback(on_focus_cheat)
 		var document: JavaScriptObject = JavaScriptBridge.get_interface("document")
 		if document != null:
-			document.addEventListener("visibilitychange", tabcheat_callback)
-			document.addEventListener("focusout", focuscheat_callback)
+			visibility_change_callback = JavaScriptBridge.create_callback(on_visibility_change)
+			document.addEventListener("visibilitychange", visibility_change_callback)
 
 func _notification(what):
 	match what:
@@ -19,8 +16,12 @@ func _notification(what):
 		MainLoop.NOTIFICATION_APPLICATION_FOCUS_OUT:
 			get_tree().paused = true
 
-func on_tab_cheat(event):
-	print("on_tab_cheat ", event)
-
-func on_focus_cheat(event):
-	print("on_focus_cheat ", event)
+func on_visibility_change(_event):
+	var document: JavaScriptObject = JavaScriptBridge.get_interface("document")
+	if document != null:
+		var visibility = JavaScriptBridge.get_interface("document").visibilityState
+		print(visibility)
+		if visibility == "hidden":
+			get_tree().paused = true
+		else:
+			get_tree().paused = false
